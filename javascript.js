0557f318ae1c12c63cdf0aefaafe68404c4c7110
Kirnic21 <marcelokenji12121@gmail.com>
2023-01-12 17:35:19 -0300

    made forms and submit button

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const passwordField=document.getElementById("pwd")
+const submitButton = document.querySelector("#submitbutton")
+submitButton.addEventListener("click",function(){
+let password=document.querySelector("#pwd").value
+let cpassword=document.querySelector("#cpwd").value
+if(password!=cpassword)
+{
+    alert("password doesnt match! try again")
+    passwordField.setCustomValidity("Invalid field.");
+}
+});
